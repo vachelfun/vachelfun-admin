32baff5b7e4ec2b295bf8988db4754c55d9701ee
vachelfun <vachelfun@gmail.com>
2023-02-26 19:44:04 +0800

    stylelint

--- src/App.vue
+++ src/App.vue
@@ -14,16 +14,18 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaaaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
